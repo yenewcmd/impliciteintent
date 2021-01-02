b182db37bca8b8853cccc1887163578351a32b4b
yenewcmd <76645571+yenewcmd@users.noreply.github.com>
2021-01-03 01:35:40 +0300

    Software Engineering

--- app/src/main/java/com/example/myfinalsassiment/MainActivity.java
+++ app/src/main/java/com/example/myfinalsassiment/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.ShareCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_PERM_CODE = 101,GALLERY_REQUEST_CODE=102;
     ImageView selectedImage;
     Button camera, gallery;
+    EditText mWebsiteEditText,mLocationEditText,mShareTextEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         selectedImage=findViewById(R.id.displayImageView);
         camera=findViewById(R.id.cameraBtn);
         gallery=findViewById(R.id.galleryBtn);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
+        mShareTextEditText = findViewById(R.id.share_edittext);
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,5 +82,45 @@ public class MainActivity extends AppCompatActivity {
             Bitmap image= (Bitmap) data.getExtras().get("data");
             selectedImage.setImageBitmap(image);
         }
+    }
+
+    public void OpenWebsit(View view) {
+        // Get the URL text.
+        String url = mWebsiteEditText.getText().toString();
+// Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+// Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void OpenLocation(View view) {
+        // Get the string indicating a location. Input is not validated; it is
+// passed to the location handler intact.
+        String loc = mLocationEditText.getText().toString();
+// Parse the location and create the intent.
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+// Find an activity to handle the intent, and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void SherText(View view) {
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_text_with)
+                .setText(txt)
+                .startChooser();
     }
 }
